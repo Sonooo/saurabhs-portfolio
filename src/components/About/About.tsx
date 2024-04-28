3e04d1a5ce8746bbca34805d1a00e801b42189e6
Sonooo <sanapsaurabh218@gmail.com>
2024-04-28 21:47:19 +0530

    small change in hero , about and contact

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -24,19 +24,7 @@ export function About() {
         </ScrollAnimation>
         
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-        <p> <b>Hello, I'm Saurabh!</b>Software developer with a knack for building web solutions that matter, using a diverse toolkit that spans the MERN stack (MongoDB, Express.js, React.js, Node.js) and extends into emerging tech like Bun, generative AI, and low-code/no-code platforms. </p>
-
-<h3>Areas of Expertise</h3>
-<p>
-<ul>
-    <li><strong>Full Stack Development:</strong> Transforms ideas into fully operational and user-friendly applications. Expertise spans from high-level system design to intricate system engineering.</li>
-    {/* <li><strong>System Designing and Problem Solving:</strong> Specializes in crafting scalable architecture and solving complex problems efficiently, enhancing both system functionality and performance.</li> */}
-    {/* <li><strong>New Technologies:</strong> Engages with cutting-edge technologies, integrating tools like <strong>Bun</strong>, <strong>AppWrite</strong>, and <strong>Supabase</strong>, alongside <strong>Firebase</strong> and <strong>generative AI</strong>, to lead in tech innovation.</li> */}
-    <li><strong>API Development:</strong> Develops reliable and fast <strong>RESTful APIs</strong> and <strong>GraphQL services</strong>, adhering to the latest security practices such as <strong>OAuth</strong> and <strong>JWT</strong>. Prioritizes optimization and thorough testing to ensure scalability and ease of maintenance.</li>
-    <li><strong>Cloud Proficiency:</strong> Proficient with <strong>AWS</strong> infrastructure tools like <strong>Lambdas</strong>, <strong>RDS</strong>, and <strong>DynamoDB</strong>; builds robust and scalable applications.</li>
-    {/* <li><strong>Innovation and Automation:</strong> Leverages the latest in AI and automation technologies to accelerate development processes and introduce next-generation solutions.</li> */}
-</ul>
-</p>
+        <p> <b>Hello, I'm Saurabh!</b> <br /> Software Developer specializes in the MERN stack, including MongoDB, Express.js, React.js, and Node.js, with proficiency in emerging technologies such as Bun, generative AI, and low-code/no-code platforms. Demonstrates strong capabilities in system design, problem-solving, and building scalable solutions using RESTful APIs, GraphQL, and AWS infrastructure. Committed to innovation, adept at integrating cutting-edge technologies to enhance development and deliver advanced applications.</p>
 
         </ScrollAnimation>
         <div className="hard-skills">
